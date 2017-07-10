9b623e8d7fd18662e53e27a2c09bf602f8bdc0ac
Ian Brennan <ian.brennan@icloud.com>
2017-07-10 15:45:28 -0600

    read me done, requests working

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import logging
 from flask import Flask, Response, json, request
 from dotenv import Dotenv
+from uuid import uuid5, uuid1
 import pymysql
 # Of course, replace by your correct path
 dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
@@ -18,53 +19,101 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def build_db():
-    conn = connect()
-    query = "create table User (ID int NOT NULL, firstName varchar(255) NOT NULL, lastName varchar(255) NOT NULL, email varchar(255), PRIMARY KEY (ID))"
-    try:
-        with conn.cursor() as cur:
-            cur.execute(query)
-            conn.commit()
-    except Exception as e:
-        logger.exception("could not build table")
-    finally:
-        cur.close()
-        conn.close()
-        response = Response(json.dumps({"message": "succes"}), 200)
-        return response
-        
+
+
     
 def connect():
     try:
         cursor = pymysql.cursors.DictCursor
         conn = pymysql.connect(RDS_HOST, user=NAME, passwd=PASSWORD, db=DB_NAME, port=RDS_PORT, cursorclass=cursor, connect_timeout=5)
         logger.info("SUCCESS: connection to RDS successful")
-        return(conn)
+        return conn 
     except Exception as e:
         logger.exception("Database Connection Error")
+
+def validate(data):
+    error_fields = []
+    not_null = [
+        "first_name",
+        "last_name",
+        "email"
+    ]
+
+    for x in not_null:
+        if x not in data or len(data[x]) == 0:
+            error_fields.append(x)
+    return (len(error_fields) == 0, error_fields)
+
+
+def build_db():
+    conn = connect()
+    with conn.cursor() as cur:
+        cur.execute("drop table if exists User")
+        cur.execute("create table User ( ID varchar(255) NOT NULL, FirstName varchar(255) NOT NULL, LastName varchar(255) NOT NULL, Email varchar(255) NOT NULL, PRIMARY KEY (ID))")
+        conn.commit()
+    return build_response({"message": "success"}, 200)
+
+def insert(data):
+    uniq_id = str(uuid5(uuid1(), str(uuid1())))
+    query = """insert into User (ID, FirstName, LastName, Email)
+            values(%s, %s, %s, %s)
+            """
+    return (query, (uniq_id, data["first_name"], data["last_name"], data["email"]))
+
+def build_response(resp_dict, status_code):
+    response = Response(json.dumps(resp_dict), status_code)
+    return response
         
 @app.route('/')
 def index():
-    return "Hello World!", 200
+    return build_response({"message": "Welcome to my lambda app!"}, 200)
 
-@app.route('/build', methods=["GET"])
+@app.route('/build')
 def build():
-    build_db()
- 
+    return build_db()
+
 @app.route('/user', methods=["GET", "POST"])
 def user():
-    resp_dict = {}
+    conn = connect()
     if request.method == "GET":
-        resp_dict = {"first_name": "John", "last_name": "doe"}
+        items = []
+        try:
+            with conn.cursor() as cur:
+                cur.execute("select * from User")
+                for row in cur:
+                    items.append(row)
+                conn.commit()
+        except Exception as e:
+            logger.info(e)
+            response = build_response({"status": "error", "message": "error getting users"}, 500)
+            return response
+        finally:
+            conn.close()
+        response = build_response({"rows": items, "status": "success"}, 200)
+        return response
     if request.method == "POST":
-        data = request.form
-        first_name = data.get("first_name", "")
-        last_name = data.get("last_name", "")
-        email = data.get("email", "")
-        resp_dict = {"first_name": first_name, "last_name": last_name, "email": email}
-    response = Response(json.dumps(resp_dict), 200)
-    return response
-    
+        data = {
+            "first_name": request.form.get("first_name", ""),
+            "last_name": request.form.get("last_name", ""),
+            "email": request.form.get("email", "")
+        }
+        valid, fields = validate(data)
+        if not valid:
+            error_fields = ', '.join(fields)
+            error_message = "Data missing from these fields: %s" %error_fields
+            return build_response({"status": "error", "message": error_message}, 400)
+        query, vals = insert(data)
+        try:
+            with conn.cursor() as cur:
+                cur.execute(query, vals)
+                conn.commit()
+        except Exception as e:
+            logger.exception("insert error")
+            return build_response({"status": "error", "message": "insert error"}, 500)
+        finally:
+            conn.close()
+            cur.close()
+        return build_response({"status": "success"}, 200)
                         
 if __name__ == '__main__':
     app.run()
